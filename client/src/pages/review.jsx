5b0f5fb1be971830defe4943c38f0d11e1dbb963
LimJooHun <jkjk8836@gmail.com>
2023-05-09 02:07:26 +0900

    feat: route 설정 및 review 페이지 생성

---
+++ client/src/pages/review.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Review = () => {
+  return <div>ㅎㅇㅎㅇsdddddddddddddddddddddddddddddddddddddddddddd</div>;
+};
+
+export default Review;
---
+++ client/src/routes/RouteList.jsx
@@ -0,0 +1,10 @@
+import Review from "../pages/review";
+
+const RouteList = [
+  {
+    path: "/review",
+    element: <Review />,
+  },
+];
+
+export default RouteList;

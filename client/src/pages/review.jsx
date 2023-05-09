2f1535254f7d68b1fb8c00e12e015f0e9719d668
LimJooHun <jkjk8836@gmail.com>
2023-05-09 22:27:53 +0900

    style: detail input을  textarea로 변경

--- client/src/pages/review.jsx
+++ client/src/pages/review.jsx
@@ -4,80 +4,6 @@ import Input from "../Component/style/Input";
 import Button from "../Component/style/button";
 import ImgBtn from "../Component/style/ImgBtn";
 
-const BasicContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const RestaurantContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 3px solid var(--black-200);
-  .res-info {
-    margin-bottom: 20px;
-    .res-title {
-      margin-bottom: 10px;
-      font-size: 28px;
-      font-weight: 600;
-    }
-  }
-  .res-sosial {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: -40px;
-    .imgBtn {
-      display: flex;
-      div {
-        margin: 0px 15px 0px 0px;
-        font-size: 22px;
-      }
-    }
-  }
-`;
-const ReviewContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  .review-font {
-    font-size: 20px;
-    margin-right: 10px;
-  }
-  .margin {
-    margin: 20px 0px 10px 0px;
-  }
-  .bold {
-    font-weight: 600;
-  }
-  .review-recommend {
-    display: flex;
-    div {
-      display: flex;
-      align-items: center;
-      margin-right: 10px;
-    }
-  }
-  .review-img {
-    width: 100px;
-  }
-`;
-const ButtonContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: right;
-  align-items: center;
-  Button {
-    margin: 0px 0px 0px 10px;
-  }
-`;
 /* 컨테이너 구성 트리 구조 
   전체 컨테이너
     > 가게 정보 컨테이너
@@ -111,6 +37,18 @@ const Review = () => {
     title: "",
     detail: "",
   });
+
+  function onTestChange() {
+    var key = window.event.keyCode;
+    // If the user has pressed enter
+    if (key === 13) {
+      document.getElementById("detail").value =
+        document.getElementById("detail").value + "\n*";
+      return false;
+    } else {
+      return true;
+    }
+  }
   return (
     <BasicContainer className="Basic-Container">
       <RestaurantContainer className="restaurant-Container">
@@ -136,7 +74,9 @@ const Review = () => {
       </RestaurantContainer>
       <ReviewContainer className="Review-Container">
         <div className="review-title">
-          <div className="review-font bold margin">리뷰 한줄평 / 제목</div>
+          <label htmlFor="title" className="review-font bold margin">
+            리뷰 한줄평 / 제목
+          </label>
           <Input
             placeholder="제목을 입력해주세요."
             id="title"
@@ -147,15 +87,15 @@ const Review = () => {
           />
         </div>
         <div className="review-detail">
-          <div className="review-font bold margin">리뷰 내용</div>
-          <Input
+          <label htmlFor="detail" className="review-font bold margin">
+            리뷰 내용
+          </label>
+          <textarea
             placeholder="내용을 입력해주세요."
             id="detail"
             name="detail"
             value={detail}
             onChange={onInputChange}
-            width="100%"
-            height="10vw"
           />
         </div>
 
@@ -180,12 +120,16 @@ const Review = () => {
         <div className="review-font bold margin">가게 만족도</div>
         <div className="review-recommend">
           <div className="review-good">
-            <div className="review-font">맛있어요</div>
-            <ImgBtn imgstyle="Like" />
+            <label htmlFor="like" className="review-font">
+              맛있어요
+            </label>
+            <ImgBtn name="like" imgstyle="Like" />
           </div>
           <div className="review-bad">
-            <div className="review-font">별로에요</div>
-            <ImgBtn imgstyle="Hate" />
+            <label htmlFor="hate" className="review-font">
+              별로에요
+            </label>
+            <ImgBtn name="hate" imgstyle="Hate" />
           </div>
         </div>
       </ReviewContainer>
@@ -198,3 +142,95 @@ const Review = () => {
 };
 
 export default Review;
+
+const BasicContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const RestaurantContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 3px solid var(--black-200);
+  .res-info {
+    margin-bottom: 20px;
+    .res-title {
+      margin-bottom: 10px;
+      font-size: var(--xx-large-font);
+      font-weight: 600;
+    }
+  }
+  .res-sosial {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: -40px;
+    .imgBtn {
+      display: flex;
+      div {
+        margin: 0px 15px 0px 0px;
+        font-size: var(--x-large-font);
+      }
+    }
+  }
+`;
+const ReviewContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .review-title,
+  .review-detail {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    textarea {
+      height: auto;
+      min-height: 200px;
+      box-sizing: border-box;
+      white-space: normal;
+      font-size: var(--medium-font);
+      border: var(--border, 1px solid var(--black-200));
+      border-radius: 10px;
+      padding: 10px;
+      resize: none;
+      overflow: hidden;
+    }
+  }
+  .review-font {
+    font-size: var(--large-font);
+    margin-right: 10px;
+  }
+  .margin {
+    margin: 20px 0px 10px 0px;
+  }
+  .bold {
+    font-weight: 600;
+  }
+  .review-recommend {
+    display: flex;
+    div {
+      display: flex;
+      align-items: center;
+      margin-right: 10px;
+    }
+  }
+  .review-img {
+    width: 100px;
+  }
+`;
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  Button {
+    margin: 0px 0px 0px 10px;
+  }
+`;

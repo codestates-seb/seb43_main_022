2f9b98e328031e98a3e412686a83a458839c259c
LimJooHun <jkjk8836@gmail.com>
2023-05-10 17:51:05 +0900

    fix : route 설정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import GlobalStyles from "./Globalstyle";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StoreList from "./Pages/StoreList";
-import AddStore from "./Pages/AddStore";
 import Header from "./Component/Header";
 import Footer from "./Component/Footer";
-import Login from "./Pages/login";
-import Signup from "./Pages/signup";
 
 import RouteList from "./routes/RouteList";
 
@@ -20,10 +16,6 @@ function App() {
           {RouteList.map((route, idx) => (
             <Route path={route.path} element={route.element} key={idx} />
           ))}
-          <Route path="/" element={<StoreList />} />
-          <Route path="/add" element={<AddStore />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
       <Footer />
--- client/src/routes/RouteList.jsx
+++ client/src/routes/RouteList.jsx
@@ -1,5 +1,11 @@
 import Main from "../pages/main";
 import Review from "../pages/review";
+import StoreList from "./Pages/StoreList";
+import AddStore from "./Pages/AddStore";
+import Login from "./Pages/login";
+import Signup from "./Pages/signup";
+import DetailPage from "../pages/DetailPage";
+import MyPage from "../pages/MyPage";
 
 const RouteList = [
   {
@@ -9,6 +15,30 @@ const RouteList = [
   {
     path: "/",
     element: <Main />,
+  },
+  {
+    path: "/itemlist",
+    element: <StoreList />,
+  },
+  {
+    path: "/addstore",
+    element: <AddStore />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/detail",
+    element: <DetailPage />,
+  },
+  {
+    path: "/mypage",
+    element: <MyPage />,
   },
 ];
 

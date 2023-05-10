36ee4ee4285389f8054928f8bf4017c7505f3efb
gjy0605 <gjy0605@gmail.com>
2023-05-10 14:14:51 +0900

    feat: 라우터 설정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,20 @@
+import React from "react";
 import GlobalStyles from "./Globalstyle";
-import styled from "styled-components";
-import Button from "./Component/style/button";
-import Input from "./Component/style/Input";
-import ImgBtn from "./Component/style/ImgBtn.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StoreList from "./Pages/StoreList";
+import AddStore from "./Pages/AddStore";
 
 function App() {
-  const Div = styled.div`
-    width: 300px;
-    height: 300px;
-  `;
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyles />
       <div className="container">
-        <Div>
-          <ImgBtn imgstyle={"Hate"} />
-        </Div>
-
-        <Button btnstyle="Btn">로그인</Button>
-        <Button btnstyle="Btn2">중식</Button>
-        <Button btnstyle="HBtn">로그인</Button>
-        <Button btnstyle="SBtn">수정</Button>
-        <Button btnstyle="SBtn2">수정</Button>
-        <Input placeholder="아이디 입력"></Input>
-        <Input inputType="error" placeholder="에러 타입"></Input>
+        <Routes>
+          <Route path="/" element={<StoreList />} />
+          <Route path="/add" element={<AddStore />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 

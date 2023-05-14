6a8ddb3f763dd017ee0077452ed7395242520405
sksjw95 <tjwldnd95@hanmail.net>
2023-05-14 23:12:29 +0900

    feat/tag-restaurant

---
+++ server/src/main/java/com/codea/tag/TagRestaurant.java
@@ -0,0 +1,43 @@
+package com.codea.tag;
+
+import com.codea.restaurant.Restaurant;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Auditable;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class TagRestaurant  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long tagRestaurantId;
+
+    @ManyToOne
+    @JoinColumn(name = "RESTAURANT_ID")
+    private Restaurant restaurant;
+
+    @ManyToOne
+    @JoinColumn(name = "TAG_ID")
+    private Tag tag;
+
+    public void setTag(Tag tag){
+        this.tag = tag;
+        if(!this.tag.getTagRestaurants().contains(this)){
+            this.tag.getTagRestaurants().add(this);
+        }
+    }
+
+    public  void setRestaurant(Restaurant restaurant){
+        this.restaurant = restaurant;
+        if(!this.restaurant.getTagRestaurants().contains(this)){
+            this.restaurant.setTagRestaurant(this);
+        }
+    }
+
+}

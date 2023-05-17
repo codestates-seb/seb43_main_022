3538bc60a9cd58febdd81d85a6045aab9c141b3c
LimJooHun <jkjk8836@gmail.com>
2023-05-18 06:14:10 +0900

    refector : review 페이지 컴포넌트화

--- client/src/Pages/Review.jsx
+++ client/src/Pages/Review.jsx
@@ -1,165 +1,51 @@
 import styled from "styled-components";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Input from "../Component/style/StyleInput";
 import Button from "../Component/style/StyleButton";
-import ImgBtn from "../Component/style/ImgBtn";
-import { useInput } from "../hooks/useInput";
+import ResInfo from "../Component/ReviewPageComp/ResInfo";
+import ReviewInfo from "../Component/ReviewPageComp/ReviewInfo";
+import { ReviewState } from "../state/atoms/ReviewAtom";
 import { api } from "../Util/api";
 
-const info = {
-  title: "하이마트",
-  view: "1.5k",
-  heart: "500",
-  tags: [
-    {
-      name: "#햄버거",
-    },
-    {
-      name: "#버거",
-    },
-  ],
-};
+const BasicContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 70px;
+`;
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  Button {
+    margin: 0px 0px 0px 10px;
+  }
+`;
 
-const Review = (restaurant_id) => {
-  const [{ title, content, img }, onInputChange] = useInput({
-    title: "",
-    content: "",
-    img: "",
-  });
-  const [, setResinfo] = useState({});
-  const [rating, setRating] = useState("");
+const Review = ({ restaurant_id }) => {
   const history = useNavigate();
-
-  api
-    .get(`/restaurants/${restaurant_id}`)
-    .then((res) => {
-      setResinfo(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
+  // 리뷰 남기기 버튼
   const handleSubmit = () => {
     api
-      .post(`/restaurants/${restaurant_id}/review`, {
-        title,
-        content,
-        img,
-        rating,
-      })
+      .post(`/restaurants/${restaurant_id}/review`, ReviewState)
       .then(() => {
         console.log("잘보냄");
       })
       .catch((err) => {
-        console.log(err);
+        console.log(`${err} 에러가 발생함`);
       });
   };
+  // 취소 버튼
   const handleCancel = () => {
     history(-1);
   };
-  const handleRating = (rating) => {
-    setRating(rating);
-    console.log(rating);
-  };
-
   return (
     <BasicContainer className="Basic-Container">
-      <RestaurantContainer className="restaurant-Container">
-        <div className="res-info">
-          <span className="res-title">{info.title}</span>
-          <ul className="tag-ul">
-            {/* 나중에 변경해야함 */}
-            {info.tags.map((tags, tagId) => (
-              <li key={tagId}>
-                <span>{tags.name}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="res-sosial">
-          <div className="imgBtn">
-            <ImgBtn imgstyle="View" />
-            <span>{info.view}</span>
-          </div>
-          <div className="imgBtn">
-            <ImgBtn imgstyle="Heart" />
-            <span>{info.heart}</span>
-          </div>
-          <div className="imgBtn">
-            <ImgBtn imgstyle="Share" />
-          </div>
-        </div>
-      </RestaurantContainer>
-      <ReviewContainer className="Review-Container">
-        <div className="review-title">
-          <label htmlFor="title" className="review-font bold margin">
-            리뷰 한줄평 / 제목
-          </label>
-          <Input
-            placeholder="제목을 입력해주세요."
-            id="title"
-            name="title"
-            value={title}
-            onChange={onInputChange}
-            inputType="default"
-            width="100%"
-          />
-        </div>
-        <div className="review-content">
-          <label htmlFor="content" className="review-font bold margin">
-            리뷰 내용
-          </label>
-          <textarea
-            placeholder="내용을 입력해주세요."
-            id="content"
-            name="content"
-            value={content}
-            onChange={onInputChange}
-          />
-        </div>
-
-        <div className="margin">
-          <img
-            className="review-img"
-            src="https://i0.wp.com/adventure.co.kr/wp-content/uploads/2020/09/no-image.jpg"
-            alt="사진이 비어있을때의 모습"
-          />
-
-          <input
-            type="image"
-            className="review-img"
-            src="https://i0.wp.com/adventure.co.kr/wp-content/uploads/2020/09/no-image.jpg"
-            alt="제출버튼"
-          />
-          {/* <label for="file" class="upload-btn">
-            <input id="file" type="file" accept="image/*" />
-          </label> */}
-        </div>
-        <div className="review-font bold margin">가게 만족도</div>
-        <div className="review-rating">
-          <div className="review-good">
-            <label htmlFor="like" className="review-font">
-              맛있어요
-            </label>
-            <ImgBtn
-              name="like"
-              imgstyle="Like"
-              onClick={() => handleRating("like")}
-            />
-          </div>
-          <div className="review-bad">
-            <label htmlFor="hate" className="review-font">
-              별로에요
-            </label>
-            <ImgBtn
-              name="hate"
-              imgstyle="Hate"
-              onClick={() => handleRating("hate")}
-            />
-          </div>
-        </div>
-      </ReviewContainer>
+      <ResInfo />
+      <ReviewInfo />
       <ButtonContainer className="Button-Container">
         <Button btnstyle="Btn" onClick={handleSubmit}>
           리뷰 남기기
@@ -173,106 +59,3 @@ const Review = (restaurant_id) => {
 };
 
 export default Review;
-
-const BasicContainer = styled.div`
-  width: 100%;
-  max-width: 1200px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-const RestaurantContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 3px solid var(--black-200);
-  .res-info {
-    margin-bottom: 20px;
-    .res-title {
-      margin-bottom: 10px;
-      font-size: var(--xx-large-font);
-      font-weight: 600;
-    }
-    .tag-ul {
-      display: flex;
-      flex-wrap: wrap;
-      li {
-        list-style: none;
-        margin: 4px 8px 0px 0px;
-        span {
-          font-size: var(--large-font);
-          color: var(--black-600);
-        }
-      }
-    }
-  }
-  .res-sosial {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: -20px;
-    .imgBtn {
-      display: flex;
-      span {
-        margin: 0px 15px 0px 0px;
-        font-size: var(--x-large-font);
-      }
-    }
-  }
-`;
-const ReviewContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  .review-title,
-  .review-content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    textarea {
-      height: auto;
-      min-height: 200px;
-      box-sizing: border-box;
-      white-space: normal;
-      font-size: var(--medium-font);
-      border: var(--border, 1px solid var(--black-200));
-      border-radius: 10px;
-      padding: 10px;
-      resize: none;
-      overflow: hidden;
-    }
-  }
-  .review-font {
-    font-size: var(--large-font);
-    margin-right: 10px;
-  }
-  .margin {
-    margin: 20px 0px 10px 0px;
-  }
-  .bold {
-    font-weight: 600;
-  }
-  .review-rating {
-    display: flex;
-    div {
-      display: flex;
-      align-items: center;
-      margin-right: 10px;
-    }
-  }
-  .review-img {
-    width: 100px;
-  }
-`;
-const ButtonContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: right;
-  align-items: center;
-  Button {
-    margin: 0px 0px 0px 10px;
-  }
-`;

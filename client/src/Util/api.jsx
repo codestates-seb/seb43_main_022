6691b5fd465d925e1d13e2cf70a468863f334570
LimJooHun <jkjk8836@gmail.com>
2023-05-18 06:10:58 +0900

    fix : api 서버 변경

--- client/src/Util/api.jsx
+++ client/src/Util/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "production"
     ? process.env.RESOUCE_URL
-    : "http://ec2-3-34-52-160.ap-northeast-2.compute.amazonaws.com:8080/";
+    : "http://ec2-54-180-31-226.ap-northeast-2.compute.amazonaws.com:8080";
 // "http://localhost:8080";
 
 const api = axios.create({

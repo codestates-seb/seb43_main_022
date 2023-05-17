4bd72306705806ba26512e0e86764ca83a5d6697
LimJooHun <jkjk8836@gmail.com>
2023-05-18 06:12:51 +0900

    feat : 카테고리 Atom 추가

---
+++ client/src/state/atoms/CategoryAtom.jsx
@@ -0,0 +1,18 @@
+import { atom } from "recoil";
+export const categoryState = atom({
+  key: "categoryState",
+  default: [
+    "한식",
+    "중식",
+    "일식",
+    "양식",
+    "베트남 요리",
+    "인도 요리",
+    "분식",
+    "디저트",
+    "퓨전요리",
+    "채식",
+    "해물",
+    "고기",
+  ],
+});

7d9ac0a730d2862a6eed12378eb39e4ce5e0d6a8
gjy0605 <gjy0605@gmail.com>
2023-05-18 13:10:00 +0900

    feat: sever 주소 공용 api컴포넌트로 활용

--- client/src/Util/api.jsx
+++ client/src/Util/api.jsx
@@ -2,9 +2,8 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? process.env.RESOUCE_URL
-    : "http://localhost:8080";
-
+    ? process.env.REACT_APP_API_URL
+    : "http://ec2-54-180-31-226.ap-northeast-2.compute.amazonaws.com:8080";
 const api = axios.create({
   baseURL,
   timeout: 3000,

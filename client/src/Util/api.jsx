d907ecf28c91de090b267b3ade374f045a61a2c6
LimJooHun <jkjk8836@gmail.com>
2023-05-21 15:27:52 +0900

    fix : api 인터셉터 추가

--- client/src/Util/api.jsx
+++ client/src/Util/api.jsx
@@ -3,13 +3,21 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "production"
     ? process.env.REACT_APP_API_URL
-    : "http://ec2-54-180-31-226.ap-northeast-2.compute.amazonaws.com:8080";
+    : "http://ec2-54-180-31-226.ap-northeast-2.compute.amazonaws.com";
 const api = axios.create({
   baseURL,
   timeout: 3000,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use(function (config) {
+  config.headers["authorization"] =
+    window.sessionStorage.getItem("Authorization");
+
+  console.log(config);
+  return config;
 });
 
 export { api };

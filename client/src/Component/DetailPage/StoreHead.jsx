fc7863a67c5d5c005493fa36f7c65d1f8d9be165
ock2023 <dmsdhr2605@gmail.com>
2023-05-24 22:13:01 +0900

    fix: 즐겨찾기 로그인 navigate 추가

--- client/src/Component/DetailPage/StoreHead.jsx
+++ client/src/Component/DetailPage/StoreHead.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import ImgBtn from "../style/ImgBtn";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import memberState from "../../state/atoms/SignAtom";
 import { api } from "../../Util/api";
@@ -46,6 +46,8 @@ const SubInfo = styled.div`
 
 const StoreHead = () => {
   const { res_id } = useParams();
+
+  const navigate = useNavigate();
   const member = useRecoilValue(memberState);
 
   const [heartIcon, setHeartIcon] = useState(false);
@@ -99,6 +101,7 @@ const StoreHead = () => {
     try {
       if (!member.memberId) {
         alert("로그인이 필요합니다.");
+        navigate("/login");
         return;
       }
       if (!heartIcon && member.memberId) {

ebdf4a9c6bd78a4ee20454f6366f5387c9a892d0
gjy0605 <gjy0605@gmail.com>
2023-05-24 13:46:23 +0900

    fix: api 주소 수정

--- client/src/Util/api.jsx
+++ client/src/Util/api.jsx
@@ -3,8 +3,10 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "production"
     ? process.env.REACT_APP_API_URL
-    : "http://ec2-54-180-31-226.ap-northeast-2.compute.amazonaws.com:8080";
-
+    : // : "https://0524-58-122-102-109.ngrok-free.app";
+      // : "http://ec2-43-202-53-65.ap-northeast-2.compute.amazonaws.com:8080";
+      // "https://6e78-58-122-102-109.ngrok-free.app";
+      "http://localhost:4003";
 const api = axios.create({
   baseURL,
   timeout: 10000,
